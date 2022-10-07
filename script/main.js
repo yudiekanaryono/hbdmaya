5e1e60a88c6ab1342c70b0d552b877fb09c8a759
Yudi Eka N <82560050+yudiekanaryono@users.noreply.github.com>
2022-10-08 01:00:17 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
  window.addEventListener('load', () => {
     Swal.fire({
-      title: 'Do you want to play music in the background?',
+      title: 'Apakah kamu ingin memutar musik di latar belakang?',
       // text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
