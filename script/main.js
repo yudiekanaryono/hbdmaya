ee2220677d2942330739d7f8b598734b0e800c16
Yudi Eka N <82560050+yudiekanaryono@users.noreply.github.com>
2022-11-03 10:18:18 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener("load", () => {
   Swal.fire({
-    title: "Mau Memainkan Musik Dilatar Belakang?",
+    title: "Want to play music in the background ?",
     // text: "You won't be able to revert this!",
     icon: "warning",
     showCancelButton: true,
